477115b89272fbb0ae33e50428b43ca8abc5bac1
SauriX <sauri618@gmail.com>
2026-03-25 21:56:18 -0500

    db

--- includes/db.php
+++ includes/db.php
@@ -1,35 +1,34 @@
 <?php
 error_reporting(0);
 
-/* $servidor="localhost";
-$usuario="root";
-$clave="root";
-$base="vendefacil_5"; */
-
 $servidor="db5020093809.hosting-data.io";
 $usuario="dbu3763371";
 $clave="vendefacil_5";
+$base="dbs15483147";
 
-    $base="dbs15483147";
+$conexion = mysqli_connect($servidor,$usuario,$clave,$base);
 
+if(!$conexion){
+    die("Error conexión: " . mysqli_connect_error());
+}
 
+/* ===== COMPATIBILIDAD MYSQL -> MYSQLI ===== */
 
+function mysql_query($query){
+    global $conexion;
+    return mysqli_query($conexion, $query);
+}
 
-/* $servidor="vendefacil.mx";
-$usuario="vendefac_diego";
-$clave="camacho";
-$base="vendefac_redcubo"; */
-/*
-JODETE HERMOSO!!!!
+function mysql_fetch_assoc($result){
+    return mysqli_fetch_assoc($result);
+}
 
+function mysql_num_rows($result){
+    return mysqli_num_rows($result);
+}
 
-*/
-/*$servidor = "epicmedia.cluster-cfh0agjg2td3.us-east-2.rds.amazonaws.com";
-$usuario = "epicmedia";
-$clave = "epicmedia";
-$base = "vendefacil_restaurante";*/
-
-
-$conexion = @mysql_connect ($servidor,$usuario,$clave) or die ("Error en conexi&oacute;n.");
-@mysql_select_db($base) or die ("No BD");
+function mysql_real_escape_string($string){
+    global $conexion;
+    return mysqli_real_escape_string($conexion, $string);
+}
 ?>
